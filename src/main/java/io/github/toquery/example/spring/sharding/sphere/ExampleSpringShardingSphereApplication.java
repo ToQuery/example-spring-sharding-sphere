d0bd214582cdedb77916c62ffa14a5716c04ce84
ToQuery <toquery@qq.com>
2022-09-14 08:56:06 +0800

    initiate

---
+++ src/main/java/io/github/toquery/example/spring/sharding/sphere/ExampleSpringShardingSphereApplication.java
@@ -0,0 +1,13 @@
+package io.github.toquery.example.spring.sharding.sphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringShardingSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringShardingSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/toquery/example/spring/sharding/sphere/ExampleSpringShardingSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.toquery.example.spring.sharding.sphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringShardingSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
